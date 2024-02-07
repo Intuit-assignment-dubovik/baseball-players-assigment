d565f69dfa06d21f8daa4f2f76624c2d1f23d236
Pavel Dubovik <pavel.djub@gmail.com>
2024-02-07 22:25:32 +0200

    feature: Add PlayerController with tests

---
+++ src/main/java/paveldubovik/baseball/player/controller/PlayerController.java
@@ -0,0 +1,34 @@
+package paveldubovik.baseball.player.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import paveldubovik.baseball.player.dto.PlayerDto;
+import paveldubovik.baseball.player.service.PlayerService;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/players")
+@RequiredArgsConstructor
+public class PlayerController {
+    private final PlayerService service;
+
+    @GetMapping
+    public ResponseEntity<List<PlayerDto>> getPlayers() {
+        log.info("Controller: get all players");
+        List<PlayerDto> players = service.getPlayers();
+        return ResponseEntity.ok(players);
+    }
+
+    @GetMapping("/{playerId}")
+    public ResponseEntity<PlayerDto> getPlayerByID(@PathVariable String playerId) {
+        log.debug("Controller: get player by id - {}", playerId);
+        return ResponseEntity.ok(service.getPlayerByID(playerId));
+    }
+}
---
+++ src/test/java/paveldubovik/baseball/player/controller/PlayerControllerTest.java
@@ -0,0 +1,113 @@
+package paveldubovik.baseball.player.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import paveldubovik.baseball.exception.PlayerNotFoundException;
+import paveldubovik.baseball.player.dto.PlayerDto;
+import paveldubovik.baseball.player.service.PlayerService;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(PlayerController.class)
+class PlayerControllerTest {
+    String playerId = "abadijo01";
+    int birthYear = 1850;
+    byte birthMonth = 11;
+    byte birthDay = 4;
+    String birthCountry = "USA";
+    String birthState = "PA";
+    String birthCity = "Philadelphia";
+    int deathYear = 1905;
+    byte deathMonth = 5;
+    byte deathDay = 17;
+    String deathCountry = "USA";
+    String deathState = "NJ";
+    String deathCity = "Pemberton";
+    String nameFirst = "John";
+    String nameLast = "Abadie";
+    String nameGiven = "John W.";
+    int weight = 192;
+    int height = 72;
+    char battingHand = 'R';
+    char throwingHand = 'R';
+    LocalDate debut = LocalDate.of(1875, 4, 26);
+    LocalDate finalGame = LocalDate.of(1875, 6, 10);
+    String retroID = "abadj101";
+    String bbrefID = "abadijo01";
+    PlayerDto expectedPlayer = new PlayerDto(playerId, birthYear, birthMonth, birthDay, birthCountry, birthState, birthCity, deathYear, deathMonth, deathDay,
+            deathCountry, deathState, deathCity, nameFirst, nameLast, nameGiven, weight, height, battingHand, throwingHand, debut, finalGame,
+            retroID, bbrefID);
+    @Autowired
+    MockMvc mockMvc;
+    @MockBean
+    PlayerService service;
+    @Autowired
+    ObjectMapper mapper;
+
+    @Test
+    @DisplayName("Controler: Get all players")
+    void testGetPlayers() throws Exception {
+        // Given
+        String expectedAsString = mapper.writeValueAsString(List.of(expectedPlayer));
+        // When
+        when(service.getPlayers()).thenReturn(List.of(expectedPlayer));
+        ResultActions result = mockMvc.perform(get("/api/players")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print());
+        // Then
+        String resultAsString = result
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString();
+        Assertions.assertEquals(expectedAsString, resultAsString);
+        verify(service, times(1)).getPlayers();
+    }
+
+    @Test
+    void getPlayerByID() throws Exception {
+        // Given
+        String expectedAsString = mapper.writeValueAsString(expectedPlayer);
+        // Then
+        when(service.getPlayerByID(playerId)).thenReturn(expectedPlayer);
+        ResultActions result = mockMvc.perform(get("/api/players/" + playerId)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print());
+        // When
+        String resultAsString = result
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString();
+        Assertions.assertEquals(expectedAsString, resultAsString);
+        verify(service, times(1)).getPlayerByID(any());
+    }
+
+    @Test
+    @Disabled
+    void getPlayerByIDNotExist() throws Exception {
+        // Given
+        String wrongId = "wrongId";
+        // Then
+        when(service.getPlayerByID(wrongId)).thenThrow(new PlayerNotFoundException(wrongId));
+        ResultActions result = mockMvc.perform(get("/api/players/" + wrongId)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print());
+        // When
+        result
+                .andExpect(status().is(404))
+                .andReturn().getResponse().getContentAsString();
+        verify(service, times(1)).getPlayerByID(any());
+    }
+}
